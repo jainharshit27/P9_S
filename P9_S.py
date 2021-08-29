1bd77f61bbeeeb3ae5545782f885483fadfa294f
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-08-29 16:08:22 +0530

    Update P9_S.py

--- P9_S.py
+++ P9_S.py
@@ -4,7 +4,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((200, 200))
 
-invisible_man = pygame.Rect(0,10,500,500)
+invisible_box = pygame.Rect(0,10,500,500)
 
 change = 10
 for i in range(100):
@@ -15,10 +15,10 @@ for i in range(100):
         pygame.quit()
         sys.exit()
     
-    pygame.draw.rect(screen, (0,0,0), invisible_man)        
+    pygame.draw.rect(screen, (0,0,0), invisible_box)        
     
-    invisible_man.x += change
-    invisible_man.y += change
+    invisible_box.x += change
+    invisible_box.y += change
     
     pygame.display.update()
     pygame.time.delay(100)
